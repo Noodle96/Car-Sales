30f871a3d628a5f778823225c3ea6a32cf6869cd
Noodle96 <jorgealfredomarino2@gmail.com>
2024-02-15 14:57:11 -0500

    Agregar la seccion de Hero

---
+++ components/Hero.tsx
@@ -0,0 +1,29 @@
+'use client';
+import { CustomButton } from "."
+
+const Hero = () => {
+	const handleScroll = () => {
+		console.log('click');
+	}
+	return (
+		<div className="hero">
+			<div className="flex-1 pt-36 padding-x">
+				<h1 className="hero__title">
+					Busca, reserva o renta un carro  -  rápido y fácil
+				</h1>
+				<p className="hero__subtitle">
+					Optimice su experiencia de alquiler de vehículos con nuestro
+					sencillo proceso de reserva
+				</p>
+				<CustomButton
+					title="Explora Carros"
+					containerStyles="bg-primary-blue text-white rounded-full mt-10"
+					handleClick={handleScroll}
+				/>
+			</div>
+		</div>
+		
+	)
+}
+
+export default Hero
