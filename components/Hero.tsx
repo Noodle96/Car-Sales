e26c7be674458d86ceee64b2fae98aff4972b650
Noodle96 <jorgealfredomarino2@gmail.com>
2024-02-15 15:09:47 -0500

    Agregar the image and the image-overlay dentro de hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Image from "next/image";
 import { CustomButton } from "."
 
 const Hero = () => {
@@ -20,6 +21,18 @@ const Hero = () => {
 					containerStyles="bg-primary-blue text-white rounded-full mt-10"
 					handleClick={handleScroll}
 				/>
+			</div>
+
+			<div className="hero__image-container">
+				<div className="hero__image">
+					<Image
+						src="/hero.png"
+						alt="hero image"
+						fill
+						className="object-contain "
+					/>
+					<div className="hero__image-overlay"></div>
+				</div>
 			</div>
 		</div>
 		
