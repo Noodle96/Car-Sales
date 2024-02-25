127dd33023577221e5b907d84c66333df96fc72b
Noodle96 <jorgealfredomarino2@gmail.com>
2024-02-24 19:41:18 -0500

    Agregar el componente SearchBarManufacturer y sus props

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,15 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchBarManufacturer from "./SearchBarManufacturer";
 export {
 	Hero,
 	CustomButton,
 	Navbar,
 	Footer,
+	CustomFilter,
+	SearchBar,
+	SearchBarManufacturer,
 };
