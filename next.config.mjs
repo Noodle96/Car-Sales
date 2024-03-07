90cc6d3942908a3017b6feb70eb008db6d4a091d
Noodle96 <jorgealfredomarino2@gmail.com>
2024-03-07 00:51:04 -0500

    config next.config.mjs to accept some hostname

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+			  protocol: 'https',
+			  hostname: 'cdn.imagin.studio',
+			  pathname: '**',
+			},
+		  ],
+		// domains: ['cdn.imagin.studio'],
+		// remotePatterns: ['cdn.imagin.studio'],
+	}
+};
 
 export default nextConfig;
